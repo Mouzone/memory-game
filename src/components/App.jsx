57801fc3007ea21193a15fc5c028cdbcefe2f36b
Mouzone <sunnyliu010@gmail.com>
2024-09-12 15:09:25 -0400

    Modify onClick for cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,46 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import '../styles/App.css'
+
+const cards = [
+    "2_of_clubs",
+    "2_of_diamonds",
+    "2_of_hearts",
+    "2_of_spades",
+]
 
 function App() {
     // todo: pick 12 random cards from cards
-    const cards = [
-        "src/assets/cards/2_of_clubs.png",
-        "src/assets/cards/2_of_diamonds.png",
-        "src/assets/cards/2_of_hearts.png",
-        "src/assets/cards/2_of_spades.png",
-    ]
+    const [best_score, setBestScore] = useState(0)
+    const [curr_score, setCurrScore] = useState(0)
+    const [seen, setSeen] = useState([])
+
+    function handleClick(link) {
+
+        const new_curr_score = seen.includes(link)
+            ? 0
+            : curr_score + 1
+        const new_best_score = Math.max(best_score, seen.includes(link)
+            ? curr_score
+            : curr_score + 1)
+        const new_seen = seen.includes(link)
+            ? []
+            : [...seen, link]
+
+        setCurrScore(new_curr_score)
+        setBestScore(new_best_score)
+        setSeen(new_seen)
+    }
 
     return <div id="cards">
             { cards.map((link, index) =>
-                <Card key={index} link={link}/>)
+                <Card key={index} link={link} handleClick={handleClick}/>)
             }
         </div>
 }
 
-function Card({link}) {
-    return <div>
-        <img src={link} alt="card"/>
+function Card({link, handleClick}) {
+    return <div onClick={() => handleClick(link)}>
+        <img src={`src/assets/cards/${link}.png`} alt="card"/>
     </div>
 }
 
